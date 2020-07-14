e81786eac7e8e7772f0b32f950dd6e004cf7df1f
Stefanos <stefanoskokkalhs@gmail.com>
2020-07-14 13:15:54 +0300

    update added

--- src/app/crud/update/update.component.ts
+++ src/app/crud/update/update.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CrudService } from '../crud.service';
+import { Product } from '../product';
+import { ActivatedRoute, Router} from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-update',
@@ -6,10 +10,53 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./update.component.css']
 })
 export class UpdateComponent implements OnInit {
+  product_id: string;
+  product:Product;
+  activatedRoute: ActivatedRoute; 
+  productForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    public crudService: CrudService,
+    activatedRoute: ActivatedRoute,
+    private router: Router,
+    public fb: FormBuilder
+    ) {
+    this.activatedRoute = activatedRoute;
+  }
+
+  submitForm() {
+    this.crudService.update(this.product_id ,this.productForm.value).subscribe(res => {
+      console.log('Product updated!')
+      this.router.navigateByUrl('')
+    })
+  }
 
   ngOnInit() {
+    this.productForm = this.fb.group({
+      name: [''],
+      description: [''],
+      price: [''],
+      quantity: ['']
+    })
+    this.activatedRoute.paramMap.subscribe(params => {
+      this.product_id = params.get('productId');
+      return this.crudService.getByID(this.product_id)
+      .subscribe(res => {
+        this.product = res;
+        console.log(res)
+        this.productForm = this.fb.group({
+          name: this.product.name,
+          description: this.product.description,
+          price: this.product.price,
+          quantity: this.product.quantity
+        })
+      })
+    })
+  }
+
+
+  ngOnDestroy() {
+    // this.routeSub.unsubscribe();
   }
 
 }
